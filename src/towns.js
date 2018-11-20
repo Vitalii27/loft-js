b8bc6f1bfeaaac93326f3b29a725df71e53e452b
Vitalii27 <v.khlystun01@gmail.com>
2018-11-20 21:56:06 +0200

    home6

---
+++ src/towns.js
@@ -0,0 +1,126 @@
+/*
+ Страница должна предварительно загрузить список городов из
+ https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ и отсортировать в алфавитном порядке.
+
+ При вводе в текстовое поле, под ним должен появляться список тех городов,
+ в названии которых, хотя бы частично, есть введенное значение.
+ Регистр символов учитываться не должен, то есть "Moscow" и "moscow" - одинаковые названия.
+
+ Во время загрузки городов, на странице должна быть надпись "Загрузка..."
+ После окончания загрузки городов, надпись исчезает и появляется текстовое поле.
+
+ Разметку смотрите в файле towns-content.hbs
+
+ Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+
+ *** Часть со звездочкой ***
+ Если загрузка городов не удалась (например, отключился интернет или сервер вернул ошибку),
+ то необходимо показать надпись "Не удалось загрузить города" и кнопку "Повторить".
+ При клике на кнопку, процесс загруки повторяется заново
+ */
+
+/*
+ homeworkContainer - это контейнер для всех ваших домашних заданий
+ Если вы создаете новые html-элементы и добавляете их на страницу, то дабавляйте их только в этот контейнер
+
+ Пример:
+   const newDiv = document.createElement('div');
+   homeworkContainer.appendChild(newDiv);
+ */
+const homeworkContainer = document.querySelector('#homework-container');
+
+/*
+ Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
+
+ Массив городов пожно получить отправив асинхронный запрос по адресу
+ https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ */
+let arrayTowns;
+
+function loadTowns() {
+    return new Promise(resolve => {
+        let url = 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json';
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', url);
+        xhr.send();
+        xhr.addEventListener('load', () => {
+            loadingBlock.style.display = 'none';
+            filterBlock.style.display = 'block';
+            let towns = JSON.parse(xhr.responseText);
+            towns = towns.sort((a, b) => {
+                if (a.name > b.name) {
+                    return 1;
+                }
+                if (a.name < b.name) {
+                    return -1;
+                }
+
+                return 0;
+            });
+            resolve(towns);
+        });
+    })
+}
+
+loadTowns().then(towns => {
+    arrayTowns = towns;
+    return arrayTowns;
+});
+
+/*
+ Функция должна проверять встречается ли подстрока chunk в строке full
+ Проверка должна происходить без учета регистра символов
+
+ Пример:
+   isMatching('Moscow', 'moscow') // true
+   isMatching('Moscow', 'mosc') // true
+   isMatching('Moscow', 'cow') // true
+   isMatching('Moscow', 'SCO') // true
+   isMatching('Moscow', 'Moscov') // false
+ */
+function isMatching(full, chunk) {
+    return full.toLowerCase().indexOf(chunk.toLowerCase()) > -1;
+}
+
+const renderTowns = (arr) => {
+    arr.forEach(town => {
+        const div = document.createElement('div');
+        div.textContent = town.name;
+        listTowns.appendChild(div);
+    })
+};
+
+/* Блок с надписью "Загрузка" */
+const loadingBlock = homeworkContainer.querySelector('#loading-block');
+/* Блок с текстовым полем и результатом поиска */
+const filterBlock = homeworkContainer.querySelector('#filter-block');
+/* Текстовое поле для поиска по городам */
+const filterInput = homeworkContainer.querySelector('#filter-input');
+/* Блок с результатами поиска */
+const filterResult = homeworkContainer.querySelector('#filter-result');
+
+const listTowns = document.createElement('div');
+listTowns.classList.add('list-towns')
+filterBlock.appendChild(listTowns)
+// console.log(towns)
+filterInput.addEventListener('keyup', function () {
+    const value = filterInput.value;
+
+    if (!value) {
+        renderTowns(arrayTowns);
+    }
+
+    const filterTowns = arrayTowns.filter(town => {
+        isMatching(town.name, value)
+    });
+
+    renderTowns(filterTowns)
+
+});
+
+export {
+    loadTowns,
+    isMatching
+};
